522e1b7752a8916d4bf5f7b4677a67545a8913f0
kohei3110 <kk.31108424@gmail.com>
2022-04-15 01:56:54 +0900

    modify az deployment group command

--- Java Containerization/scripts/deploy.sh
+++ Java Containerization/scripts/deploy.sh
@@ -4,5 +4,5 @@ REGION=$1
 RG_NAME=$2
 
 az group create --name $RG_NAME --location $REGION
-az group deployment create --name "azuredeploy" --resource-group $RG_NAME --template-file "./azuredeploy.json" --parameters "./azuredeploy.parameters.json" --verbose
+az deployment group create --name "azuredeploy" --resource-group $RG_NAME --template-file "./azuredeploy.json" --parameters "./azuredeploy.parameters.json" --verbose
 
